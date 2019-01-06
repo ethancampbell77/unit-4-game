94afce52604f5840b9bc6398d557f7f5245d4cc1
CW Wolf <44934371+ethancampbell77@users.noreply.github.com>
2019-01-06 15:54:59 -0500

    3rd commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+// Collector Game Javascript //
+
+// Need a randomly generated Number between 19 and 120 Math.random//
+// Push number to variable and then to P id="compNum" with getElement? //
+
+// Need a score card to log wins and losses //
+// Use if/else or switch to asses wins and losses 
+// if (userGuess === compNum) { userWin ++1, reset} else if(userGuess > compNum) { userLose ++1, reset} //
+
+// 4 Items need value between 1 and 12 each - 4 separate math,randoms in 1 fuction? // 
+// push to item variables then getElement to HTML //
+
+// Need a player score captured in a variable and then getElement to HTML//
