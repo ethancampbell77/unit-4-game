cbc37cc91c06f53292a833f9543f7901d7c2d508
CW Wolf <44934371+ethancampbell77@users.noreply.github.com>
2019-01-07 18:13:08 -0500

    getting closer

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,127 @@
 // Collector Game Javascript //
 
-// Need a randomly generated Number between 19 and 120 Math.random//
-// Push number to variable and then to P id="compNum" with getElement? //
+$(document).ready(function() {
 
-// Need a score card to log wins and losses //
-// Use if/else or switch to asses wins and losses 
-// if (userGuess === compNum) { userWin ++1, reset} else if(userGuess > compNum) { userLose ++1, reset} //
+// Random Value Generated Computer Number
+var compRand = Math.floor(Math.random()*101+19);
+    console.log(compRand);
+    $("#compNum").text(compRand);
 
-// 4 Items need value between 1 and 12 each - 4 separate math,randoms in 1 fuction? // 
-// push to item variables then getElement to HTML //
+// Random Value Generated Items //
+    var itemNum1 = Math.floor(Math.random()*11+1);
+    var itemNum2 = Math.floor(Math.random()*11+1);
+    var itemNum3 = Math.floor(Math.random()*11+1);
+    var itemNum4 = Math.floor(Math.random()*11+1);
 
-// Need a player score captured in a variable and then getElement to HTML//
+    var winner = 0;
+    var loser = 0;
+    var userScore = 0;
+
+    $("#wins").text("WINS: " + winner);
+    $("#losses").text("LOSSES: " + loser);
+
+// First Item Score Increments //
+    $("#stone1").on("click",function() {
+    userScore = userScore + itemNum1;
+    console.log(userScore);
+    $("#playerTotal").text(userScore);
+
+    if(userScore === compRand) {
+        youWin();
+    }
+
+    else if(userScore > compRand) {
+        youLose();
+    }
+    });
+
+// Second Item Score Increments //
+    $("#stone2").on("click", function() {
+    userScore = userScore + itemNum2;
+    $("#playerTotal").text(userScore);
+    console.log(userScore);
+    
+    if(userScore === compRand) {
+        youWin();
+    }
+    
+    else if(userScore > compRand) {
+        youLose();
+    }
+    });
+
+ // Third Item Score Increments //   
+    $("#stone3").on("click", function() {
+    userScore = userScore + itemNum3;
+    $("#playerTotal").text(userScore);
+    console.log(userScore);
+           
+    if(userScore === compRand) {
+        youWin();
+    }
+            
+    else if(userScore > compRand) {
+        youLose();
+    }
+    });
+
+// Fourth Item Score Increments //
+    $("#stone4").on("click", function() {
+    userScore = userScore + itemNum4;
+    $("#playerTotal").text(userScore);
+    console.log(userScore);
+               
+    if(userScore === compRand) {
+        youWin();
+    }
+                
+    else if(userScore > compRand) {
+        youLose();
+    }
+    });
+
+ // WIN function //
+    function youWin() {
+        winner ++;
+        $("#wins").text(winner);
+        gameReset();
+    }
+
+// LOSE funtion //
+    function youLose() {
+        loser ++;
+        $("#losses").text(loser);
+        gameReset();
+    }
+
+// Game Reset - Generates Computer Number, All 4 Item Numbers and resets Player Score to 0 //
+
+    function gameReset() {
+    var compRand = Math.floor(Math.random()*101+19);
+    console.log(compRand);
+    $(compNum).text(compRand);
+    var itemNum1 = Math.floor(Math.random()*11+1);
+    var itemNum2 = Math.floor(Math.random()*11+1);
+    var itemNum3 = Math.floor(Math.random()*11+1);
+    var itemNum4 = Math.floor(Math.random()*11+1);
+    userScore = 0
+    $("#playerTotal").text(userScore);
+}
+
+});   
+
+  
+
+
+
+
+
+
+
+
+
+
+    
+    
+
+
