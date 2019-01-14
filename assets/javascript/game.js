10d7f4f824eb0c0ac9a32b6897f6d9f8cc89d823
CW Wolf <44934371+ethancampbell77@users.noreply.github.com>
2019-01-13 20:34:56 -0500

    finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -17,98 +17,79 @@ var compRand = Math.floor(Math.random()*101+19);
     var loser = 0;
     var userScore = 0;
 
-    $("#wins").text("WINS: " + winner);
-    $("#losses").text("LOSSES: " + loser);
-
 // First Item Score Increments //
-    $("#stone1").on("click",function() {
-    userScore = userScore + itemNum1;
+    $("#pinkDonut").on("click",function() {
+    userScore += itemNum1;
     console.log(userScore);
     $("#playerTotal").text(userScore);
-
-    if(userScore === compRand) {
-        youWin();
-    }
-
-    else if(userScore > compRand) {
-        youLose();
-    }
+    gameScores();
     });
 
 // Second Item Score Increments //
-    $("#stone2").on("click", function() {
-    userScore = userScore + itemNum2;
+    $("#chocDonut").on("click", function() {
+    userScore += itemNum2;
     $("#playerTotal").text(userScore);
     console.log(userScore);
-    
-    if(userScore === compRand) {
-        youWin();
-    }
-    
-    else if(userScore > compRand) {
-        youLose();
-    }
+    gameScores();
     });
+    
 
  // Third Item Score Increments //   
-    $("#stone3").on("click", function() {
-    userScore = userScore + itemNum3;
+    $("#greenDonut").on("click", function() {
+    userScore += itemNum3;
     $("#playerTotal").text(userScore);
     console.log(userScore);
+    gameScores();
+    });
            
+// Fourth Item Score Increments //
+    $("#blueDonut").on("click", function() {
+    userScore += itemNum4;
+    $("#playerTotal").text(userScore);
+    console.log(userScore);
+    gameScores();
+    });
+
+    function gameScores() {
     if(userScore === compRand) {
-        youWin();
+        winner++;
+        document.getElementById("winMusic").play();
+        gameReset();
     }
             
     else if(userScore > compRand) {
-        youLose();
-    }
-    });
-
-// Fourth Item Score Increments //
-    $("#stone4").on("click", function() {
-    userScore = userScore + itemNum4;
-    $("#playerTotal").text(userScore);
-    console.log(userScore);
-               
-    if(userScore === compRand) {
-        youWin();
-    }
-                
-    else if(userScore > compRand) {
-        youLose();
-    }
-    });
-
- // WIN function //
-    function youWin() {
-        winner ++;
-        $("#wins").text(winner);
+        loser++;
+        document.getElementById("loseMusic").play();
         gameReset();
-    }
+      }
+    };
+    
 
-// LOSE funtion //
-    function youLose() {
-        loser ++;
-        $("#losses").text(loser);
-        gameReset();
-    }
-
-// Game Reset - Generates Computer Number, All 4 Item Numbers and resets Player Score to 0 //
+// Game Reset - Generates New Computer Number, New Values for All 4 Item Numbers, Adds to Wins/Losses and resets Player Score to 0 //
 
     function gameReset() {
-    var compRand = Math.floor(Math.random()*101+19);
-    console.log(compRand);
-    $(compNum).text(compRand);
-    var itemNum1 = Math.floor(Math.random()*11+1);
-    var itemNum2 = Math.floor(Math.random()*11+1);
-    var itemNum3 = Math.floor(Math.random()*11+1);
-    var itemNum4 = Math.floor(Math.random()*11+1);
     userScore = 0
-    $("#playerTotal").text(userScore);
-}
+    compRand = Math.floor(Math.random()*101+19);
+    console.log(compRand);
+    $("#compNum").text(compRand);
+    itemNum1 = Math.floor(Math.random()*11+1);
+    itemNum2 = Math.floor(Math.random()*11+1);
+    itemNum3 = Math.floor(Math.random()*11+1);
+    itemNum4 = Math.floor(Math.random()*11+1);
+    $("#wins").text("WINS: " + winner);
+    $("#losses").text("LOSSES: " + loser);
+    }
 
-});   
+// Theme Music If for Chrome & Else for the rest of the browsers //
+    var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+    if(!isChrome){
+    $('#iframeAudio').remove()
+    }
+    else{
+    $('#playAudio').remove() 
+    }
+
+});  
 
   
 
